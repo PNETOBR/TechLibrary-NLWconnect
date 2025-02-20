975cee36b6057b5ba2c4ad745895ba8778d18ff5
PNETOBR <princenetobr@gmail.com>
2025-02-20 08:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ TechLibrary.API/UseCases/Users/Register/RegisterUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using TechLibrary.Communication.Requests;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserValidator : AbstractValidator<RequestUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(request => request.Name).NotEmpty().WithMessage("O nome é obrigatorio.");
+        RuleFor(request => request.Email).EmailAddress().WithMessage("O Email não é valido.");
+        RuleFor(request => request.Password).NotEmpty().WithMessage("A Senha é obrigatorio.");
+        When(request => string.IsNullOrEmpty(request.Password) == false, () =>
+        {
+            RuleFor(request => request.Password.Length).GreaterThanOrEqualTo(6).WithMessage("A Senha deve conter mais que 6 caracteres");
+        });
+
+        //NotEmpyt valida se o campo é nulo ou vazio / when é a função anonima que valida se a senha é nula ou vazia
+    }
+}
